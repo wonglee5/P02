c5759a702306b25786d21441db5f3fda46c4cf05
wonglee5 <clw14@sfu.ca>
2016-11-05 00:21:51 -0700

    Edited the Nav to include mobile

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -3,19 +3,15 @@
 // with zepto script (shorter ver. b/c predefined code)
 var width = $(window).width();
 
-if ( width < 500 ) {
+if ( width < 500) {
+	$(".navMain").addClass("hidden");
+	$(".navMainMobile").removeClass("hidden");
 
-	$("#navToggle").removeClass("hidden");
-	$("#navCollapse").addClass("hidden");
-
-	//changing attributes of nav-toggle
 	$("#navToggle").attr("aria-hidden", "false");
 	$("#navCollapse").attr("aria-hidden", "true");
 }
 
-
-$("#navToggle").click( 
-	function() {
+$("#navToggle").click(function() {
 		$("#navCollapse").toggleClass("hidden");
 
 		if ( $("#navCollapse").hasClass("hidden") ) {
@@ -27,3 +23,28 @@ $("#navToggle").click(
 
 	}
 );
+
+// if ( width < 500 ) {
+
+// 	$("#navToggle").removeClass("hidden");
+// 	$("#navCollapse").addClass("hidden");
+
+// 	//changing attributes of nav-toggle
+// 	$("#navToggle").attr("aria-hidden", "false");
+// 	$("#navCollapse").attr("aria-hidden", "true");
+// }
+
+
+// $("#navToggle").click( 
+// 	function() {
+// 		$("#navCollapse").toggleClass("hidden");
+
+// 		if ( $("#navCollapse").hasClass("hidden") ) {
+// 			$("#navCollapse").attr("aria-hidden", "true");
+// 		} else {
+// 			$("#navCollapse").attr("aria-hidden", "false");
+// 		}
+
+
+// 	}
+// );
