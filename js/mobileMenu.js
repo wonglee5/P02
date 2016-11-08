134a6304bc39b2465f9b8970a911205cceebf328
SooJin Lee <rorinsjl@gmail.com>
2016-11-07 16:12:21 -0800

    Organized and added citations. Few minor changes.

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -23,28 +23,3 @@ $("#navToggle").click(function() {
 
 	}
 );
-
-// if ( width < 500 ) {
-
-// 	$("#navToggle").removeClass("hidden");
-// 	$("#navCollapse").addClass("hidden");
-
-// 	//changing attributes of nav-toggle
-// 	$("#navToggle").attr("aria-hidden", "false");
-// 	$("#navCollapse").attr("aria-hidden", "true");
-// }
-
-
-// $("#navToggle").click( 
-// 	function() {
-// 		$("#navCollapse").toggleClass("hidden");
-
-// 		if ( $("#navCollapse").hasClass("hidden") ) {
-// 			$("#navCollapse").attr("aria-hidden", "true");
-// 		} else {
-// 			$("#navCollapse").attr("aria-hidden", "false");
-// 		}
-
-
-// 	}
-// );
